4a3b4b396cebde288a44ff6ef8dee3ba64b4dfba
Renato Castro <renatocastro02@hotmail.com>
2020-07-28 14:34:34 -0300

    Input_Button

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Text, StatusBar } from 'react-native';
 import Background from '~/pages/components/Background';
+import Input from '~/pages/components/Input';
+import Button from '~/pages/components/Button';
 
 // import { Container } from './styles';
 
@@ -9,6 +11,12 @@ const SignIn = () => {
     <Background>
       <StatusBar translucent backgroundColor="transparent" />
       <Text> Hello World </Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite o seu nome"
+      />
+      <Button>Entrar</Button>
     </Background>
   );
 };
