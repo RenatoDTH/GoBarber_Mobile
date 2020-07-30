52df5a5df9d7219bba4e13c3ec37fc58eb1cb654
Renato Castro <renatocastro02@hotmail.com>
2020-07-30 17:55:32 -0300

    Rota Inicial

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,41 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import SignIn from './pages/SignIn/index';
-import SignUp from './pages/SignUp/index';
+import SignIn from '~/pages/SignIn/index';
+import SignUp from '~/pages/SignUp/index';
+import Dashboard from '~/pages/Dashboard/index';
 
-const AppStack = createStackNavigator();
+const AuthStack = createStackNavigator();
+const AuthStackScreen = () => (
+  <AuthStack.Navigator
+    screenOptions={{
+      headerShown: false,
+    }}
+  >
+    <AuthStack.Screen name="SignIn" component={SignIn} />
+    <AuthStack.Screen name="SignUp" component={SignUp} />
+  </AuthStack.Navigator>
+);
+
+const AppStack = createBottomTabNavigator();
+const AppStackScreen = () => (
+  <AppStack.Navigator
+    screenOptions={{
+      headerShown: false,
+    }}
+  >
+    <AuthStack.Screen name="Dashboard" component={Dashboard} />
+  </AppStack.Navigator>
+);
 
 export default function Routes() {
+  const signed = useSelector((state) => state.auth.signed);
   return (
     <NavigationContainer>
-      <AppStack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <AppStack.Screen name="SignIn" component={SignIn} />
-        <AppStack.Screen name="SignUp" component={SignUp} />
-      </AppStack.Navigator>
+      {signed ? <AppStackScreen /> : <AuthStackScreen />}
     </NavigationContainer>
   );
 }
