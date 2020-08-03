e99d428f6a3a0486de8adc4e49004b23e23768e6
Renato Castro <renatocastro02@hotmail.com>
2020-08-03 14:26:15 -0300

    DatePicker por plataforma

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Background from '~/pages/components/Background/index';
+import DateInput from '~/pages/components/DateInput';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 const SelectDateTime = () => {
-  return <Background />;
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <Background>
+      <Container>
+        <DateInput date={date} onChange={setDate} />
+      </Container>
+    </Background>
+  );
 };
 
 export default SelectDateTime;
